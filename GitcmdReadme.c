1f21603e8528ff99c3eee2dade5f56c8017b0133
Xipan Zhu <xpzhu@chiponeic.com>
2017-11-09 11:24:34 +0800

    [add] add 'git clean'

--- GitcmdReadme.c
+++ GitcmdReadme.c
@@ -18,10 +18,16 @@
    git format-patch HEAD^^^^^ 	//<==不支持！！！！error！！
 3> git reset --hard "branchname"//回退版本
 4> patch -p1 < 0001-#######.patch //打补丁
+
 /***git 经过验证的打包命令***/
 1> (这一步是别人做)git format-patch -s "branchname" //生成patch,"branchname" 为git log 倒数第二个commit id
 2> git apply --reject "0001-test.patch" //应用patch 包，可能有错，warning不用管;如果没有错，测试应该是更改了；有错，需要手动更改
 3> git add .   "或者其他add" 
 4> git commit -m "commit"
+
+/***git 使用***/
+1> git clean -dxf  //命令用来从你的工作目录中删除所有没有tracked过的文件
+	-df 删除 文件 和 目录
+	-xf删除当前目录下所有没有track过的文件. 不管他是否是.gitignore文件里面指定的文件夹和文件.
 
  
